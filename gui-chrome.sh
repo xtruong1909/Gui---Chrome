bcf177d1ec0519348f1a1d3c0ccc1ae026847aac
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-10-26 20:08:58 +0700

    Create gui-chrome.sh

---
+++ gui-chrome.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Cài đặt Openbox và TightVNC server
+sudo apt install openbox tightvncserver -y
+
+# Tạo thư mục và thiết lập mật khẩu VNC
+mkdir -p ~/.vnc
+echo "$PASSVNCSERVER" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
+
+# Dừng VNC server nếu đang chạy
+vncserver -kill :1
+
+# Tạo file cấu hình xstartup
+cat <<EOF > ~/.vnc/xstartup
+#!/bin/sh
+
+xrdb "$HOME/.Xresources"
+xsetroot -solid "#ADD8E6"
+export XKL_XMODMAP_DISABLE=1
+/etc/X11/Xsession
+xsetroot -cursor_name left_ptr &
+openbox-session &
+EOF
+
+chmod +x ~/.vnc/xstartup
+
+# Khởi động lại VNC server
+vncserver :100 -geometry 1100x710 -depth 24
+
+# Cài đặt Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
